5d0a9d1f976b71b7f4a0b076b75b77c0b19c3819
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2024-09-04 21:24:41 +0930

    Initial Commit

---
+++ app/src/main/java/com/example/fuelcalculator/FuelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fuelcalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FuelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fuel)
+    }
+}
---
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.fuelcalculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+
+        btnStart.setOnClickListener {
+
+            val intent = Intent(this, FuelActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
